d3af82714cb4e2211a2599ad33575d5ac6b99e78
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-16 22:30:36 +0530

    SystemUIClocks: Import from husky-user-14-UD1A.230803.041-10808477-release-keys
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Idf799a370e3152d4d2a8046aaa69aa7b79f62214

---
+++ product.mk
@@ -0,0 +1,12 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/SystemUIClocks
+
+# Clocks
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-NumOverlap \
+    SystemUIClocks-Weather
